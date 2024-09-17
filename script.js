b35876ac23c8d6acec6c6d71efed9d00783c81f6
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-09-17 22:46:40 +0100

    basic functions to handle calculations created

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(para1, para2){
+    let result = para1 + para2;
+    return result;
+    
+}
+
+function sub(para1, para2){
+    let result = para1 - para2;
+    return result;
+    
+}
+
+function mul(para1, para2){
+    let result = para1 * para2;
+    return result;
+    
+}
+
+function div(para1, para2){
+    let result = para1 / para2;
+    return result;
+    
+}
