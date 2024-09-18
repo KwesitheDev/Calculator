93a3b6c4bb1a3d17efd0390114ea0e9040f06d9a
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2024-09-18 19:17:38 +0100

    operate function created, operation function return directly

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let operand1;
+let operand2;
+let operator;
+
+//Operate function
+
+function operate(operand1, operand2, operator) {
+    switch (operator) {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return sub(operand1, operand2);
+        case '*':
+            return mul(operand1, operand2);
+        case '/':
+            return div(operand1, operand2);
+        default:
+            console.log("Invalid operand or operator");
+            return null;
+    }
+}
+
 function add(para1, para2){
     let result = para1 + para2;
     return result;
@@ -17,7 +39,11 @@ function mul(para1, para2){
 }
 
 function div(para1, para2){
-    let result = para1 / para2;
-    return result;
+      
+    if (para2 === 0) {
+        console.log("Error: Division by zero");
+        return null;
+    }
+    return para1/para2;
     
 }
